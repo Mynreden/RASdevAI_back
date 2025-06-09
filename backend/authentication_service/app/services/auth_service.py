3158291b5b84e78549956ae27ff0a1556adc097e
Sultan A <aubakirov2004sultan@gmail.com>
2025-06-10 00:36:14 +0500

    added Error codes for auth handling

--- backend/authentication_service/app/services/auth_service.py
+++ backend/authentication_service/app/services/auth_service.py
@@ -54,7 +54,7 @@ class AuthService:
         return jwt.encode(to_encode, self.VERIFICATION_SECRET_KEY, algorithm=self.ALGORITHM)
 
     async def register_user(self, user: UserCreate) -> Dict:
-        result = await self.db.execute(select(User).where(User.email == user.email))
+        result = await self.db.execute(select(User).where(User.email == user.email or User.username == user.username))
         existing_user = result.scalars().first()
         if existing_user:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
@@ -97,7 +97,7 @@ class AuthService:
         if not user or not user.hashed_password or not self.verify_password(password, user.hashed_password):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
         if not user.email_verified:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Email not verified")
+            raise HTTPException(status_code=status.HTTP_423_LOCKED, detail="Email not verified")
 
         access_token = self.create_access_token({"sub": user.email})
         refresh_token = self.create_refresh_token({"sub": user.email})
